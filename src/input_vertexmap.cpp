d8593b7dfa9ea61da36b4ef05ba4572861358a5f
christian512 <c.staufe@posteo.net>
2021-03-13 13:12:33 +0100

    fix function name

--- src/input_vertexmap.cpp
+++ src/input_vertexmap.cpp
@@ -17,14 +17,14 @@
 using namespace panda;
 
 namespace {
-   VertexMap vertexmap(std::istream &);
+   VertexMap vertexmap(std::istream&);
 }
 
 VertexMaps panda::input::implementation::vertexMaps(std::istream &stream) {
    VertexMaps vertex_maps;
    std::string token;
    if (!std::getline(stream, token) || !isKeywordVertexMaps(trimWhitespace(token))) {
-      throw std::invalid_argument("Cannot read vertexMaps: file is at an invalid position.")
+      throw std::invalid_argument("Cannot read vertexMaps: file is at an invalid position.");
    }
    while (stream && !isKeyword(trimWhitespace(peekLine(stream)))) {
       if (trimWhitespace(peekLine(stream)).empty()) {
@@ -50,40 +50,7 @@ VertexMaps panda::input::implementation::vertexMaps(std::istream &stream) {
 }
 
 namespace {
-/* TODO: To be deleted
-   /// Parses a string of the form x1-2x3+4x4-1 or similar.
-   Image image(std::string token, const Names& names)
-   {
-      if ( token.empty() )
-      {
-         throw std::invalid_argument("Invalid token (is empty).");
-      }
-      Image result;
-      std::size_t position = 0;
-      while ( position != std::string::npos && position < token.size() )
-      {
-         const auto factor = extractFactor(token, position);
-         const auto variable = extractName(token, position);
-         if ( variable.empty() )
-         {
-            // not referring to a name => index == number of names
-            result.push_back(std::make_pair(names.size(), factor));
-         }
-         else
-         {
-            const auto iterator = std::find(names.cbegin(), names.cend(), variable);
-            if ( iterator == names.cend() )
-            {
-               throw std::invalid_argument("In \"" + token + "\" of a map, the token \"" + variable + "\" is not a variable.");
-            }
-            const auto index = iterator - names.cbegin();
-            result.push_back(std::make_pair(index, factor));
-         }
-      }
-      return result;
-   }
-*/
-   VertexMap vertexMap(std::istream &stream) {
+   VertexMap vertexmap(std::istream& stream) {
       using namespace input;
       VertexMap vertex_map;
       // we assume that each entry has length of number of vertices provided
