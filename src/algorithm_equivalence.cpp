24170f2530a0ab44e7fca87a46feea661418e35c
christian512 <c.staufe@posteo.net>
2021-03-16 11:52:23 +0100

    initial equivalence check implementation

--- src/algorithm_equivalence.cpp
+++ src/algorithm_equivalence.cpp
@@ -5,20 +5,42 @@
 //-------------------------------------------------------------------------------//
 
 #define COMPILE_TEMPLATE_ALGORITHM_CLASSES
+
 #include "algorithm_equivalence.h"
+
 #undef COMPILE_TEMPLATE_ALGORITHM_CLASSES
 
-#include <algorithm>
-#include <cassert>
-#include <cstddef>
 #include <set>
-#include <tuple>
-#include <vector>
 
 
 using namespace panda;
 
-bool panda::algorithm::equivalencePolyToPoly(const std::vector<int>& indicesVertexPolyOne, const std::vector<int>& indicesVertexPolyTwo, const VertexMaps& vertex_maps)
-{
-   return true;
+bool panda::algorithm::equivalencePolyToPoly(std::set<int> indicesVertexPolyOne, std::set<int> indicesVertexPolyTwo,
+                                             const VertexMaps &vertex_maps) {
+   // Check the number of the vertices
+   if (indicesVertexPolyOne.size() != indicesVertexPolyTwo.size()) {
+      return false;
+   }
+   // As both lists are sets (i.e. ordered) we can use the equality operator
+   if (indicesVertexPolyOne == indicesVertexPolyTwo) {
+      return true;
+   }
+   for (const auto &vertex_map : vertex_maps) {
+      auto newIndicesVertexPolyTwo = mapVerticesPolytope(indicesVertexPolyTwo, vertex_map);
+      if (newIndicesVertexPolyTwo == indicesVertexPolyOne) {
+         return true;
+      }
+   }
+   return false;
+}
+
+std::set<int> panda::algorithm::mapVerticesPolytope(std::set<int> indicesVerticesPoly, const VertexMap &vertex_map) {
+   for (int i = 0; i < vertex_map.size(); i++) {
+      const auto pos = indicesVerticesPoly.find(i);
+      if (pos != indicesVerticesPoly.end()) {
+         indicesVerticesPoly.erase(i);
+         indicesVerticesPoly.insert(vertex_map[i]);
+      }
+   }
+   return indicesVerticesPoly;
 }
--- src/algorithm_equivalence.h
+++ src/algorithm_equivalence.h
@@ -7,6 +7,7 @@
 #pragma once
 
 #include "maps.h"
+#include <set>
 
 namespace panda
 {
@@ -15,9 +16,9 @@ namespace panda
       /// Checks if two polytopes are equivalent under the given vertex maps
       /// IF under a vertex map the vertices of one polytope can be mapped to the vertices of the other
       /// both polytopes are considered equal
-      bool equivalencePolyToPoly(const std::vector<int>&, const std::vector<int>&, const VertexMaps&);
+      bool equivalencePolyToPoly(std::set<int>, std::set<int>, const VertexMaps&);
+      /// Function to map the indices of a polytope under a vertex map
+      std::set<int> mapVerticesPolytope(std::set<int>, const VertexMap&);
    }
 }
-
-/// #include "algorithm_equivalence.eti"
 
