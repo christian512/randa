bcbfaf4ad9ebaa153650a55f91111a83cae6ad96
christian512 <c.staufe@posteo.net>
2023-01-23 18:11:46 +0100

    add type definitions

---
+++ src/symmetries.h
@@ -0,0 +1,16 @@
+
+//-------------------------------------------------------------------------------//
+// Author: Christian Staufenbiel                                                 //
+// License: CC BY-NC 4.0 http://creativecommons.org/licenses/by-nc/4.0/legalcode //
+//-------------------------------------------------------------------------------//
+
+#pragma once
+#include <vector>
+#include <string>
+namespace panda
+{
+    /// Single symmetry
+    using Symmetry = std::string;
+    /// Multiple symmetries
+    using Symmetries = std::vector<Symmetry>;
+}
